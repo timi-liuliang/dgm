7d715abd7158d3a40aa14cf0acd202d04d0c5994
captain <qq79402005@gmail.com>
2025-08-08 01:46:50 +0800

    config pages

---
+++ dgm.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+# navigation
+# https://getemoji.com/
+pg = st.navigation(
+    {   
+        "DGM":
+        [
+            st.Page("page/home/home.py", title="Home", icon="💻"),
+        ],
+        "Project" : 
+        [
+            st.Page("page/benchmark/benchmark.py", title="Benchmark", icon="🐳"),
+        ],
+        "Infrastructure" : 
+        [
+            st.Page("page/cpfs/cpfs.py", title="Cpfs", icon="🍄"),
+        ]
+    },
+    expanded=True,
+    position="sidebar"
+)
+
+pg.run()
